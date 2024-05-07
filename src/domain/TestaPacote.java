7dfee0242d769aea46efbec158a35c8b5da2587c
LaisAlonso <01242521925@edu.udesc.br>
2024-05-07 13:53:01 -0300

    Classe Pessoa e respectivo teste dentro do mesmo pacote

---
+++ src/domain/TestaPacote.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class TestaPacote {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+
+        p.nome = " Zé da Silva ";
+
+        System.out.printf("Nome: " + p.nome);
+    }
+}
