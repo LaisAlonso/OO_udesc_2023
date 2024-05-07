ef7c142345e521eedddf2e87b1937fcdf9c4f880
LaisAlonso <01242521925@edu.udesc.br>
2024-05-07 15:00:30 -0300

    Modificação para atributos privados na classe Pessoa

--- src/domain/TestaPacote.java
+++ src/domain/TestaPacote.java
@@ -2,10 +2,11 @@ package domain;
 
 public class TestaPacote {
     public static void main(String[] args) {
+
         Pessoa p = new Pessoa();
 
-        p.nome = " Zé da Silva ";
-
-        System.out.printf("Nome: " + p.nome);
+         //Utilizando metodos get\set
+        p.setNome("Zé da Silva ");
+        System.out.printf("Nome: " + p.getNome());
     }
 }
