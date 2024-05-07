0d18b2de8e0586f9fc8e4b629a116653d76420eb
LaisAlonso <01242521925@edu.udesc.br>
2024-05-07 16:16:46 -0300

    Implementação de construtores na classe Pessoa

--- src/app/ProgramaPrincipal.java
+++ src/app/ProgramaPrincipal.java
@@ -7,12 +7,27 @@ import java.time.LocalDate;
 
 public class ProgramaPrincipal {
     public static void main(String[] args) {
+        // Utilizando o construtor padrão
         Pessoa fulano = new Pessoa();
         fulano.setNome( " Fulano da Silva " );
         fulano.setEndereco("Rua dos programadores, 42");
         fulano.setNascimento ( LocalDate.of(1990,10, 13));
         fulano.setSalario( new BigDecimal("1000.99"));
-
         fulano.apresentarse();
+
+        System.out.println();
+
+        // Utilizando o construtor parametrizado
+        Pessoa beltrano = new Pessoa("Beltrano da Silva",
+                "Rua dos Programadores, 666",
+                LocalDate.of(2000,2,15),
+                new BigDecimal("1234.56")
+                );
+                beltrano.apresentarse();
+
+                System.out.println();
+
+                System.out.println(fulano);
+                System.out.println(beltrano);
     }
 }
